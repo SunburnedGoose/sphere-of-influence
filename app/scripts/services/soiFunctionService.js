5cf290ffebc9af02eabf0bea125136c75fe49eb5
Rick Casey <rick.casey@gmail.com>
2014-02-19 19:07:05 -0600

    Modified the placement of the planets.

--- app/scripts/services/soiFunctionService.js
+++ app/scripts/services/soiFunctionService.js
@@ -89,8 +89,8 @@ angular.module('sphereOfInfluenceApp')
       var v = viewport();
 
       return {
-        'horizontal': v.width / 1920,
-        'vertical': v.height / 1200
+        'horizontal': v.width / 1280,
+        'vertical': v.height / 720
       };
     };
 
