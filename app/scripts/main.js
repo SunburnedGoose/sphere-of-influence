a43738de4874ba07105539e6a9f27dcc9e083cfb
Rick Casey <rick.casey@gmail.com>
2014-04-07 02:23:55 -0500

    Add touch support.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,7 @@ var tilesprite;
 var speed;
 var map;
 var layer;
+var planetoid;
 
 function create() {
   speed = 10;
@@ -26,7 +27,8 @@ function create() {
 
   game.physics.p2.convertTilemap(map, layer);
 
-  ship = game.add.sprite(800, 800, 'ship');
+  planetoid = game.add.sprite(900, 900, 'planetoid');
+  ship = game.add.sprite(600, 800, 'ship');
   ship.animations.add('forward',[1,2,3,4,5,6],6,true);
   ship.animations.add('reverse',[7,8,9,10,11,12],6,true);
   ship.animations.add('unpowered',[0],0,false);
@@ -35,10 +37,12 @@ function create() {
   game.physics.p2.applyDamping = false;
   game.physics.p2.applyGravity = false;
 
-  ship.anchor.setTo(0.5, 0.5);
-  ship.scale.setTo(0.5, 0.5);
+  //ship.anchor.setTo(0.25, 0.25);
+  ship.scale.setTo(0.25, 0.25);
+  planetoid.scale.setTo(0.75,0.75);
 
-  game.physics.p2.enable(ship);
+  game.physics.p2.enable([ship]);
+
 
   // Make the default camera follow the ufo.
   game.camera.follow(ship);
@@ -48,10 +52,33 @@ function create() {
   downKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
   leftKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
   rightKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+
+  game.input.addPointer();
+  game.input.onDown.add(changeVector);
+  game.input.onHold.add(changeVector);
+}
+
+function changeVector(a) {
+  ship.body.velocity.x = ship.body.velocity.y = 0;
+  var x1 = 400;
+  var y1 = 300;
+  var x2 = a.position.x;
+  var y2 = a.position.y;
+
+  var deltaX = x2 - x1;
+  var deltaY = y2 - y1;
+
+  var degrees = Math.atan2(deltaY, deltaX) * 180 / Math.PI + 90;
+
+  var rotation = Phaser.Math.degToRad(degrees);
+
+  ship.body.rotation = rotation;
+  update();
 }
 
 function preload() {
   game.load.image('stars', 'assets/imgs/playspace.png');
+  game.load.image('planetoid', 'assets/imgs/planetoid.png');
   //game.load.image('ship', 'assets/imgs/ship.png');
   game.load.spritesheet('ship', 'assets/imgs/ship-sprite.png', 75, 85, 13);
   game.load.tilemap('map', 'assets/tilemaps/map.json', null, Phaser.Tilemap.TILED_JSON);
@@ -62,26 +89,26 @@ function update() {
 
   if (cursors.left.isDown || leftKey.isDown)
   {
-      ship.body.rotateLeft(100);
+      ship.body.rotateLeft(speed * 10);
   }
   else if (cursors.right.isDown || rightKey.isDown)
   {
-      ship.body.rotateRight(100);
+      ship.body.rotateRight(speed * 10);
   }
   else
   {
       ship.body.setZeroRotation();
   }
 
-  if (cursors.up.isDown || upKey.isDown)
+  if (cursors.up.isDown || upKey.isDown || game.input.pointer1.isDown || game.input.mousePointer.isDown)
   {
-      ship.body.thrust(400);
+      ship.body.thrust(speed * 8);
       ship.animations.play('forward');
       thrusting = true;
   }
   else if (cursors.down.isDown || downKey.isDown)
   {
-      ship.body.reverse(100);
+      ship.body.reverse(speed * 2);
       thrusting = true;
       ship.animations.play('reverse');
   }
@@ -95,5 +122,9 @@ function update() {
     if (Math.abs(ship.body.velocity.y) < 0.4) {
       ship.body.velocity.y = 0;
     }
+  }
+
+  if (planetoid.isCamera) {
+    planetoid.body.velocity.x = planetoid.body.velocity.y = 0;
   }
 }
